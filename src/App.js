1412516f285415dc83f3a8f27ac6adc257e7f532
heinminthant <heinminthant.17199@gmail.com>
2022-04-17 11:55:17 +0630

    Added ClassPackCard component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import ClassPackCard from "./components/ClassPackCard";
 import { getClassPacks } from "./features/classPackSlice";
+import classPack from "./services/classPack";
 
 function App() {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const classPacks = useSelector((state) => state.classPack.data);
 
   useEffect(() => {
     dispatch(getClassPacks());
   }, []);
+
   return (
-    <div className=" grid grid-cols-3">
-      <div className=" flex flex-col text-center border p-2">
-        <p className=" self-start mt-2 ml-2 bg-black py-1 px-4 text-white inline-block text-sm">
-          Popular
-        </p>
-        <h3 className=" text-blue-800 text-md font-semibold">10 Class Pack</h3>
-        <p className="my-6">Class Description 1234124124124124214124124</p>
-        <p className="mb-4 font-semibold">$220.00</p>
-      </div>
+    <div className="w-screen h-screen bg-gray-100">
+      <nav className="bg-white p-4 mb-4">
+        <div className="container mx-auto flex justify-between">
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/about" className="ml-4">About</Link>
+          </div>
+          <div>
+            <Link to="/logout">Logout</Link>
+          </div>
+        </div>
+      </nav>
+      <main className=" grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 container mx-auto">
+        {classPacks.map((classPack) => (
+          <ClassPackCard key={classPack.pack_id} classPack={classPack} />
+        ))}
+      </main>
     </div>
   );
 }
---
+++ src/components/ClassPackCard.js
@@ -0,0 +1,23 @@
+import classPackIcon from "../assets/images/eight.png";
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const ClassPackCard = ({ classPack }) => {
+  return (
+    <Link to="/order" className=" flex flex-col text-center items-center border p-2 bg-white">
+      {classPack.tag_name && (
+        <p className=" self-start mt-2 ml-2 bg-black py-1 px-4 text-white inline-block text-sm">
+          {classPack.tag_name}
+        </p>
+      )}
+      <h3 className=" text-blue-800 text-md font-semibold">
+        {classPack.pack_name}
+      </h3>
+      <img className="w-16 h-16 mt-4" src={classPackIcon} />
+      <p className="my-6">{classPack.pack_description}</p>
+      <p className="mb-4 font-semibold">$ {classPack.pack_price}</p>
+    </Link>
+  );
+};
+
+export default React.memo(ClassPackCard);
